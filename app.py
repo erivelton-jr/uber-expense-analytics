82a02ddc0ad4e27744b8f1f701e1dc7d410231e7
Erivelton Junior <jrlxzz01@gmail.com>
2025-07-06 20:05:27 -0300

    refactor: update file uploader and data processing for Uber expense analytics

--- app.py
+++ app.py
@@ -1,47 +1,68 @@
-import io
+from turtledemo.penrose import start
 import streamlit as st
-import pandas as pd
 from data_extraction import extract_data_from_pdf
-from datetime import datetime
+import pandas as pd
+import numpy as np
+import plotly.express as px
 
 
-st.title("Uber Expense Analytics")
-st.write("Carregue arquivos PDF para extrair os dados de valor, quilômetros percorridos e data.")
+st.set_page_config(
+    page_title="Uber Expense Analytics",
+    page_icon="🚕",
+    layout="wide",
+    initial_sidebar_state="expanded")
 
-uploaded_files = st.file_uploader("Selecione seus recibos em PDF", accept_multiple_files=True, type="pdf")
+st.title("📊 Análise de Despesas com Uber")
 
-if uploaded_files:
-    all_data = []
-    for uploaded_file in uploaded_files:
-        st.write("Carregando arquivo:", uploaded_file.name)
-        pdf_data = extract_data_from_pdf(uploaded_file)
-        if pdf_data:
-            # Extrair o identificador do nome do arquivo
-            file_id = uploaded_file.name.split(".")[0]
-            for row in pdf_data:
-                row.append(file_id)
+uploaded_file = st.file_uploader("Envie o arquivo exportado da Uber (.csv)", type="csv")
 
-            all_data.extend(pdf_data)
+if uploaded_file:
+    try:
+        #transformação dos dados
+        df = pd.read_csv(uploaded_file)
+        df['request_time'] = pd.to_datetime(df['request_time'])
+        df['fare_amount'] = pd.to_numeric(df['fare_amount'], errors='coerce')
+        df['distance'] = pd.to_numeric(df['distance'], errors='coerce')
+        df['product_type'] = df['product_type'].str.title()
+
+        # extrair somente corridas concluídas
+        df_completed = df[df['status'] == 'completed']
+        st.success("✅ Dados carregados com sucesso!")
 
 
-    if all_data:
-        df = pd.DataFrame(all_data, columns=["total_price", "date","kilometers_traveled", "file_id"])
-        df = df.drop_duplicates().reset_index(drop=True)
-        st.write("Dados extraídos com sucesso!")
+        # Filtros
+        with st.sidebar:
+            st.header("📊 Filtros do Dashboard")
+            st.markdown("Use os filtros abaixo para personalizar a visualização dos dados.")
+            year = sorted(df_completed['request_time'].dt.year.unique().tolist())
+            year = st.sidebar.multiselect("Ano", options=year, default=year)
+            st.sidebar.markdown("---")
+            product_type = sorted(df_completed['product_type'].dropna().astype(str).unique().tolist())
+            trip_type = st.sidebar.multiselect("Tipo de corrida", options=product_type, default=product_type)
 
-        df_ready = st.dataframe(df)
 
-        output = io.BytesIO()
-        with pd.ExcelWriter(output, engine="openpyxl") as writer:
-            df.to_excel(writer, index=False)
+        # Métricas principais
+        df_filtered = df_completed[(df_completed['request_time'].dt.year.isin(year)) &
+                                    (df_completed['product_type'].isin(trip_type))]
 
-        excel_saved = output.getvalue()
+        st.metric("💰 Total Gasto", f"R$ {df_filtered['fare_amount'].sum():,.2f}")
+        st.metric("🚗 Total Corridas", df_filtered.shape[0])
+        st.metric("📍 Total Km", f"{df_filtered['distance'].sum():,.2f} km")
+        st.metric("🚗 Corrida mais cara", f"R$ {df_filtered['fare_amount'].max():,.2f}")
 
-        st.download_button(
-            label="Download Excel",
-            data=excel_saved,
-            file_name=f"uber_expense_dataset_{datetime.now()}.xlsx",
-            mime="application/vnd.ms-excel"
-        )
-    else:
-        st.write("Não foi possivel transformar os dados")
+        # Gastos por mes
+        df_filtered['month'] = df_filtered['request_time'].dt.to_period('M')
+        monthly_expenses = df_filtered.groupby('month')['fare_amount'].sum().reset_index()
+        monthly_expenses['month'] = monthly_expenses['month'].dt.to_timestamp()
+        st.subheader("📈 Gastos Mensais")
+        st.bar_chart(monthly_expenses.set_index('month'))
+
+        st.markdown("---")
+
+        # Exibir tabela filtrada
+        st.subheader("📊 Tabela de Corridas Filtradas")
+        st.dataframe(df_completed)
+
+
+    except Exception as e:
+        st.error(f"❌ Erro ao carregar o arquivo: {e}")
